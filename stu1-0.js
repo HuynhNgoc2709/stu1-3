3eb4a03ed69f908ee2f8f896368315eaf5abce8a
KhaCodder <114870280+KhaCodder@users.noreply.github.com>
2023-06-13 00:37:33 +0700

    Update stu1-0.js

--- stu1-0.js
+++ stu1-0.js
@@ -104,7 +104,7 @@ function stuProgress(e) {
 // xu ly click btn
 function handleStu(id) {
 	idElmt = getid(id);
-	synSTUbtn('ADD', 'link_4sub_com', location.hostname);
+	synSTUbtn('ADD', 'link_4sub_com', linkSTU);
 	synSTUbtn('ADD', id, idElmt.innerText);
 	remCt(idElmt, 'loader');
 	addCt(idElmt, 'done');
